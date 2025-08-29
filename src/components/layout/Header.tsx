38e0153ff242fe49aeb5f919db47a90fb46a5fe4
YearaChoi <22200750@handong.ac.kr>
2025-08-29 23:48:57 +0900

    feat: 마이페이지, 로고 클릭시 페이지 이동

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { lightTheme } from "../../styles/theme";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <Logo onClick={() => navigate('/')}>My Best Trip</Logo>
+      <Menus>
+        <Menu>여행지 밸런스 게임</Menu>
+        <Menu>반전여행지</Menu>
+        <Menu onClick={() => navigate('/mypage')}>마이페이지</Menu>
+      </Menus>
+    </Container>
+  );
+};
+
+export default Header;
+
+const Container = styled.div`
+    width: 100%;
+    height: 64px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    justify-content: space-between;
+    border-bottom: 1px solid ${lightTheme.colors.gray.light};
+    background-color: white;
+`
+
+const Logo = styled.div`
+    font-size: 22px;
+    font-weight: 500;
+    color: black;
+    margin-left: 82px;
+    cursor: pointer;
+`
+
+const Menus = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-right: 82px;
+`
+
+const Menu = styled.div`
+    font-size: 15px;
+    font-weight: 400;
+    color: black;
+    margin-left: 32px;
+    cursor: pointer;
+`
