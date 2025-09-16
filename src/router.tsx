2f66ae1cf5df582cf845f706aa96e60572325049
codms0201 <chanyeol0615@naver.com>
2025-09-17 00:39:36 +0900

    feat: reverse page add

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import MyPage from "./pages/MyPage";
 import DestinationInfo from "./pages/DestinationInfo";
 import BalanceGamePage from "./pages/BalanceGamePage";
+import ReversePage from "./pages/ReversePage";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
       {
         path: "/balance-game",
         element: <BalanceGamePage />,
+      },
+      {
+        path: "/reverse",
+        element: <ReversePage />,
       },
     ],
   },
